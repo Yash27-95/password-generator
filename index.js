b2ff4cb40fb93eaa28b8f33e107e76cac8cae497
Yash27-95 <yashwanth.ks27@gmail.com>
2024-12-09 15:13:28 +0530

    password copy feature added
    made few changes to add the functionality

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let password1El = document.getElementById("password1")
-let password2El = document.getElementById("password2")
+let password1El = document.getElementById("pass1")
+let password2El = document.getElementById("pass2")
 
 function generatePassword() {
 let pass1 = []
@@ -15,10 +15,28 @@ let pass2 = []
         let randomNum2 = Math.floor(Math.random() * characters.length)
         pass2.push(characters[randomNum2])
    }
-   password1El.textContent = pass1.join("")
+   password1El.textContent = pass1.join("") 
    password2El.textContent = pass2.join("")
 }
 
-// console.log(generatePassword())
+function copyPass1() {
+    let pass1Text = document.getElementById("pass1").textContent
+    if(pass1Text) {
+        navigator.clipboard.writeText(pass1Text)
+        alert("Copied to clipboard")   
+    }else{
+        alert("Please generate passward")
+    }
+}
+
+function copyPass2() {
+    let pass2Text = document.getElementById("pass2").textContent
+    if(pass2Text) {
+        navigator.clipboard.writeText(pass2Text)
+        alert("Copied to clipboard")   
+    }else{
+        alert("Please generate passward")
+    }
+}
 
 
